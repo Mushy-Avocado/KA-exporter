2711b4a2976b6c1c7d8d44d133ca923f5f30d60c
Mushy Avocado <79934091+Mushy-Avocado@users.noreply.github.com>
2023-11-14 17:31:02 -0700

    Update importer.js

--- importer.js
+++ importer.js
@@ -279,7 +279,7 @@
     var loadedSounds = {};
     processing.getSound = function(source) {
       if (window.location.href.includes("kasandbox.org"))
-	    	source = "https://cdn.kastatic.org/third_party/javascript-khansrc/live-editor/build/sounds/" + source + ".png";
+	    	source = "https://cdn.kastatic.org/third_party/javascript-khansrc/live-editor/build/sounds/" + source;
       else if (!source.startsWith(processing.assetRoot + 'sounds'))
         source = processing.assetRoot + 'sounds/' + source;
       if (!hasFileExtension(source))
